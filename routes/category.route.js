ec9aedf2c760928fec580b4f14220e6adc16989d
git12nazneen <lipinazneen65@gmail.com>
2024-08-25 00:30:14 +0600

    added category crud

--- routes/category.route.js
+++ routes/category.route.js
@@ -1,6 +1,10 @@
 import express from "express";
-import { createCategory } from "../controllers/category.controller.js";
+import { createCategory, deleteCategory, getAllCategory, getCategoryById, updateCategory } from "../controllers/category.controller..js";
 
 const router = express.Router();
-
+// Get /api/category
 router.post("/add-category", createCategory);
+router.get("/all-category", getAllCategory);
+router.get("/category/:id", getCategoryById);
+router.post("/update/:id", updateCategory);
+router.use("/delete/:id", deleteCategory);
