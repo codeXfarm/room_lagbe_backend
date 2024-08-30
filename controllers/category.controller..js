ead77861d5dcf7a704e75a8a1086e08982f5193f
Smais Shawon <smaisshawon@gmail.com>
2024-08-31 01:05:04 +0600

    add filter api

--- controllers/category.controller..js
+++ controllers/category.controller..js
@@ -18,55 +18,57 @@ export const createCategory = async (req, res) => {
   }
 };
 
-export const getAllCategory = async (req, res)=>{
+export const getAllCategory = async (req, res) => {
   try {
     const categories = await Category.find({ available: true });
     res.json({ success: true, categories });
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
-}
+};
 
 export const getCategoryById = async (req, res) => {
-  try{
+  try {
     const catergory = await Category.findById(req.params.id);
-    if(!catergory){
+    if (!catergory) {
       return res
-      .status(404)
-      .json({success:false, message:'Category not found'});
+        .status(404)
+        .json({ success: false, message: "Category not found" });
     }
     res.json({ success: true, catergory });
-  }catch(error){
-    return res.status(500).json({success:false, message:error.message});
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
-}
+};
 
-export const updateCategory = async (req, res) => { 
-  try{
-    const catergory = await Category.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators: true});
-    if(!catergory){
+export const updateCategory = async (req, res) => {
+  try {
+    const catergory = await Category.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!catergory) {
       return res
-     .status(404)
-     .json({success:false, message:'Category not found'});
+        .status(404)
+        .json({ success: false, message: "Category not found" });
     }
     res.json({ success: true, catergory });
-  }catch(error){
-    return res.status(400).json({success:false, message:error.message});
+  } catch (error) {
+    return res.status(400).json({ success: false, message: error.message });
   }
-}
+};
 
-
-export const deleteCategory = async (req, res)=>{
-  try{
+export const deleteCategory = async (req, res) => {
+  try {
     const category = await Category.findByIdAndDelete(req.params.id);
-    if(!category){
+    if (!category) {
       return res
-      .status(404)
-      .json({success:false, message:'Category not found'});
+        .status(404)
+        .json({ success: false, message: "Category not found" });
     }
-    res.json({success:true, message:'Room deleted successfully'});
+    res.json({ success: true, message: "Room deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
   }
-  catch(error){
-    return res.status(500).json({success:false, message:error.message});
-  }
-}
+};
